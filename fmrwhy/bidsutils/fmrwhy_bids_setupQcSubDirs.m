094416d600aaa298095d8646f87915489fc2185e
Luis Eudave <38909206+negatoscope@users.noreply.github.com>
2021-05-25 13:37:51 +0200

    Update fmrwhy_bids_setupQcSubDirs.m
    changed contains for strfind
    adapted folder directions in exchange for dir.folder missing output in octave

--- fmrwhy/bidsutils/fmrwhy_bids_setupQcSubDirs.m
+++ fmrwhy/bidsutils/fmrwhy_bids_setupQcSubDirs.m
@@ -28,13 +28,14 @@ if ~exist(options.sub_dir_preproc, 'dir')
     %   - copy or gunzip (nii.gz) all
     for i = 1:numel(dir_contents)
         % If sessions exist
-        if (dir_contents(i).isdir == 1) && contains(dir_contents(i).name, 'ses')
-            sesdir_contents = dir(fullfile(dir_contents(i).folder, dir_contents(i).name));
+        if (dir_contents(i).isdir == 1) && strfind(dir_contents(i).name, 'ses')
+            sesdir_contents = dir(fullfile(options.sub_dir_BIDS, dir_contents(i).name));
+            sesdir_file = fullfile(options.sub_dir_BIDS, dir_contents(i).name)
             % Loop through session dir
             for j = 1:numel(sesdir_contents)
                 % if there are anat or func directories, copy/gunzip files accordingly
-                if (sesdir_contents(j).isdir == 1) && (contains(sesdir_contents(j).name, 'anat') || contains(sesdir_contents(j).name, 'func'))
-                    anatfunc_dir = fullfile(sesdir_contents(j).folder, sesdir_contents(j).name);
+                if (sesdir_contents(j).isdir == 1) && (strfind(sesdir_contents(j).name, 'anat') || strfind(sesdir_contents(j).name, 'func'))
+                    anatfunc_dir = fullfile(sesdir_file, sesdir_contents(j).name);
                     deriv_subdir = fullfile(options.sub_dir_preproc, dir_contents(i).name, sesdir_contents(j).name);
                     copyGunzipDirContents(anatfunc_dir, deriv_subdir);
                 else
@@ -42,8 +43,8 @@ if ~exist(options.sub_dir_preproc, 'dir')
                 end
             end
         % If sessions DO NOT exist
-        elseif (dir_contents(i).isdir == 1) && (contains(dir_contents(i).name, 'anat') || contains(dir_contents(i).name, 'func'))
-            anatfunc_dir = fullfile(dir_contents(i).folder, dir_contents(i).name);
+        elseif (dir_contents(i).isdir == 1) && (strfind(dir_contents(i).name, 'anat') || strfind(dir_contents(i).name, 'func'))
+            anatfunc_dir = fullfile(options.sub_dir_BIDS, dir_contents(i).name);
             deriv_subdir = fullfile(options.sub_dir_preproc, dir_contents(i).name);
             copyGunzipDirContents(anatfunc_dir, deriv_subdir);
         else
@@ -60,18 +61,18 @@ if ~exist(options.sub_dir_qc, 'dir')
     
     for i = 1:numel(dir_contents)
         % If sessions exist
-        if (dir_contents(i).isdir == 1) && contains(dir_contents(i).name, 'ses')
+        if (dir_contents(i).isdir == 1) && strfind(dir_contents(i).name, 'ses')
             % create session dir
             mkdir(fullfile(options.sub_dir_qc, dir_contents(i).name));
             % Loop through session dir
             for j = 1:numel(sesdir_contents)
                 % if there are anat or func directories, copy/gunzip files accordingly
-                if (sesdir_contents(j).isdir == 1) && (contains(sesdir_contents(j).name, 'anat') || contains(sesdir_contents(j).name, 'func'))
+                if (sesdir_contents(j).isdir == 1) && (strfind(sesdir_contents(j).name, 'anat') || strfind(sesdir_contents(j).name, 'func'))
                     mkdir(fullfile(options.sub_dir_qc, dir_contents(i).name, sesdir_contents(j).name));
                 end
             end
         % If sessions DO NOT exist
-        elseif (dir_contents(i).isdir == 1) && (contains(dir_contents(i).name, 'anat') || contains(dir_contents(i).name, 'func'))
+        elseif (dir_contents(i).isdir == 1) && (strfind(dir_contents(i).name, 'anat') || strfind(dir_contents(i).name, 'func'))
             mkdir(fullfile(options.sub_dir_qc, dir_contents(i).name));
         else
             % Ignore other file types
@@ -85,9 +86,9 @@ function copyGunzipDirContents(srcDir, destDir)
     srcDir_contents = dir(srcDir);
     % assignin('base', 'srcDir_contents', srcDir_contents);
     for i = 1:numel(srcDir_contents)
-        srcDir_file = fullfile(srcDir_contents(i).folder, srcDir_contents(i).name);
-        if contains(srcDir_contents(i).name, 'sub')
-            if contains(srcDir_contents(i).name, 'nii.gz')
+        srcDir_file = fullfile(srcDir, srcDir_contents(i).name);
+        if strfind(srcDir_contents(i).name, 'sub')
+            if strfind(srcDir_contents(i).name, 'nii.gz')
                 gunzip(srcDir_file, destDir);
             else
                 if ~exist(destDir, 'dir')
